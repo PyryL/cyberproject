fa1fe9e58b47c6cd34902fbc521fbc1d2fe8cd65
Pyry Lahtinen <pyry@pyry.info>
2023-10-26 16:24:04 +0300

    clarify instructions related to the hash vulnerability

--- utilities/hash.py
+++ utilities/hash.py
@@ -3,14 +3,16 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 def generate_hash(string: str) -> str:
     # INTENTIONAL VULNERABILITY
-    # Hash algorithm MD5 is broken and should not be used in storing passwords
+    # after enabling/disabling this vulnerability, run `setup_db.py` to reset the database
+    # MD5 hash algorithm is broken and should not be used in storing passwords
     return hashlib.md5(string.encode("utf-8")).hexdigest()
-    # FIX (comment the line above and uncomment the code below)
+    # FIX (comment the line above and uncomment the line below)
     # return generate_password_hash(string)
 
 def check_hash(string: str, hash: str) -> bool:
     # INTENTIONAL VULNERABILITY CONTINUES
-    # change this according to vulnerability in `generate_hash` function
+    # (change this according to vulnerability in `generate_hash` function above)
     return generate_hash(string) == hash
-    # FIX
+    # FIX (uncomment the code line below and comment the vulnerable line above)
+    # (enable this fix only if the fix in `generate_hash` function has also been enabled)
     # return check_password_hash(hash, string)
